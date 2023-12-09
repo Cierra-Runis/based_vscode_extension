c3bb20634a234af0d89fba69729ef2895d430db9
Cierra_Runis <2864283875@qq.com>
2023-12-09 11:30:11 +0800

    📖Improve PubLatestVersion hover

--- src/hovers/pubLatestVersion.ts
+++ src/hovers/pubLatestVersion.ts
@@ -13,21 +13,46 @@ export class PubLatestVersion implements Hover {
       token: vscode.CancellationToken
     ) => {
       const line = document.lineAt(position.line).text;
+
+      if (line.trim() === "sdk: flutter") {
+        return;
+      }
+
       const reg = RegExp("(.*?): (.*)");
       const match = line.match(reg);
-      if (match !== null) {
-        const name = match[1].trim();
-        const version = match[2].trim();
-        const latestVersion = await Pubspec.getPackageLatestVersion(name);
-        if (latestVersion) {
-          return new vscode.Hover([
-            `🚀 Latest version of [${name}](https://pub.dev/packages/${name}) is [${latestVersion}](https://pub.dev/packages/${name}/versions/${latestVersion}) 🚀`,
-            version.match(latestVersion)
-              ? `🔥 You are using the latest version! 🔥`
-              : `🧊 Please consider to update it! 🧊`,
-          ]);
-        }
+      if (match === null) {
+        return;
       }
+
+      const name = match[1].trim();
+      const version = match[2].trim();
+      if (version.length === 0) {
+        return;
+      }
+
+      const latestVersion = await Pubspec.getPackageLatestVersion(name);
+
+      if (latestVersion === undefined) {
+        return;
+      }
+
+      const re = RegExp(/^\^(\d+\.\d+\.\d+(?:[+-]\S+)?)$/);
+      const showFormat = re.exec(version);
+
+      const details =
+        showFormat === null
+          ? [
+              `🧊 Please consider to use [Caret syntax](https://dart.dev/tools/pub/dependencies#caret-syntax). 🧊`,
+              `👀 See [Use caret syntax](https://dart.dev/tools/pub/dependencies#use-caret-syntax) in [Best practices](https://dart.dev/tools/pub/dependencies#best-practices). 👀`,
+            ]
+          : showFormat[0] === version
+          ? [`🔥 You are using the latest version! 🔥`]
+          : [`🧊 Please consider to update it! 🧊`];
+
+      return new vscode.Hover([
+        `🚀 Latest version of [${name}](https://pub.dev/packages/${name}) is [${latestVersion}](https://pub.dev/packages/${name}/changlog) 🚀`,
+        ...details,
+      ]);
     },
   };
 }
